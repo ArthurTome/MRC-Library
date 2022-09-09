8654308830e808811377afafd6b12116088c510a
Arthur Tome <arthurtomelopes@gmail.com>
2022-09-09 00:46:05 -0300

    Criação dos arquivos e edição do README.md

---
+++ src/MRC.cpp
@@ -0,0 +1,34 @@
+template <class T>
+class MRC{
+private:
+    /* data */
+public:
+    /* data */
+};
+
+/// @brief 
+/// @param  
+/// @return 
+int sum_sinusoids(int N)
+{
+    int i = 0;
+    while(i < N)
+    {
+        /*real-valued deterministic (SOS) Gaussian process*/
+    }
+    return 0;
+}
+
+/* Rayleigh process */
+int rayleigh ()
+{
+
+    return 0;
+}
+
+/* Rice process */
+int rice (){
+
+    return 0;
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// class templates
+#include <iostream>
+using namespace std;
+
+int main () {
+  cout << "Hello World\n";
+
+return 0;
+}
