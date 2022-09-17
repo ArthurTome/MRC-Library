a57698a778c264da65c071f66df36833be46fb4d
Arthur Tome <arthurtomelopes@gmail.com>
2022-09-17 18:13:06 -0300

    Fix class and functions and reorder archives of project

--- src/MRC.hpp
+++ src/MRC.hpp
@@ -1,3 +1,8 @@
+#include <random>
+#include <complex>
+#include <numbers>
+
+
 #define PI 3.14159265358979323846
 
 template <class datat, short N>
@@ -17,10 +22,66 @@ class SoC{
 public:
     /* functions */
     void Calc_SoC();
-    void Define_Vision_Comp(datat T_mod, datat T_freq, datat T_phas);
-//    void Define_Vector_Size(short v_size);
-    void Calc_EMEDS(double sig, double Fmax);
-    void Calc_LPNM(double sig, double Fmax);
-    datat Get();         // Channel 
-
+    void Define_vision(datat T_mod, datat T_freq, datat T_phas);
+    void Comp_model(datat sig, datat Fmax);     //EMEDS
+    void Comp_model(datat sig);                 //LPNM
+    //void Comp_model(datat Fmax);                //
 };
+
+/// @brief Compute Sum of Cisoids model
+template <class datat, short N>
+void SoC<datat, N>::Calc_SoC()
+{
+
+    short i = 1;
+    while (i <= N)
+    {
+        mu[i] += mod[i] * exp(0, 2 * PI * freq[i] + phas[i]);
+    }
+}
+
+
+template <class datat, short N>
+void SoC<datat, N>::Define_vision(datat T_mod, datat T_freq, datat T_phas)
+{
+    m_mod = T_mod;
+    m_freq = T_freq;
+    m_phas = T_phas;
+}
+
+/// @brief Compute Parameters by Extended Method of Exact Doppler Spread (EMEDS)
+/// 
+/// @param Fmax Maximum Doppler Frequency
+/// @param sig Standard Deviation
+template <class datat, short N>
+void SoC<datat, N>::Comp_model(datat sig, datat Fmax)
+{
+    // Fmax = 91 and sig = 1, text paramters p:223 fig:5.69
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<datat> distrib(0, 2 * 3.14);
+
+    short i = 1;
+    while (i <= N)
+    {
+        mod[i] = sig * sqrt(2 / N);
+        freq[i] = Fmax * cos((2 * PI * (i - 0.25) / N));
+        phas[i] = distrib(gen);
+    }
+}
+
+/// @brief Compute Parameters by Lp-Norm Method (LPNM) 
+/// @param sig Standard Deviation
+template <class datat, short N>
+void SoC<datat, N>::Comp_model(datat sig)
+{
+    short i = 1;
+    double sum_mod = 0;
+
+    while (i <= N - 1)
+    {
+        sum_mod += mod[i] * mod[i];
+    }
+
+    mod[N] = sqrt(2 * sig * sig - sum_mod);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,40 @@
+/*
 #include <iostream>
 #include "MRC.hpp"
 
+
 int main(int argc, char *argv[])
 {
-  SoC <double, 10>  channel;
-  channel.Calc_EMEDS(97, 1);
-  channel.Calc_SoC();
-  //channel.Get();
+	SoC <float, 10>channel;
+	channel.Comp_model(1, 91);
+    channel.Calc_SoC();
+
 
   return 0;
 }
+
+*/
+
+#include <complex>
+#include <random>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    random_device rd;  //Will be used to obtain a seed for the random number engine
+    mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    uniform_real_distribution<float> distrib(0, 2*3.14);
+
+    complex<float> c;
+    complex<float> c1(5, 6);
+    complex<float> c2(3, 5);
+
+    for (int n = 0; n < 10; ++n) {
+        c = (4, distrib(gen));
+        cout << c << ' ';
+    }
+    cout << '\n';
+}
+
